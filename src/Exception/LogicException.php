1b20d999e6b7b00cbdeec25954addb4b48e9f9ad
kenjis <kenji.uui@gmail.com>
2015-04-11 15:29:15 +0900

    php/skeleton

---
+++ src/Exception/LogicException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file is part of the NagoyaPHP.Vol9
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace NagoyaPHP\Vol9\Exception;
+
+class LogicException extends \LogicException implements ExceptionInterface
+{
+}
---
+++ src/Exception/RuntimeException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file is part of the NagoyaPHP.Vol9
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ */
+namespace NagoyaPHP\Vol9\Exception;
+
+class RuntimeException extends \LogicException implements ExceptionInterface
+{
+}
